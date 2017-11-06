8ec3427749cc247cebd36273f9fe6da6072d2a16
skgwt6v <more_than_extend@yahoo.co.jp>
2017-11-06 13:31:54 +0900

    表題追加

--- touch_csv.rb
+++ touch_csv.rb
@@ -11,23 +11,24 @@ require 'JSON'
 require './instagram_urls.rb'
 require './scrape_instagram.rb'
 
-codes = []
-textes = []
-dates = []
-views = []
-likes = []
+codes = ["url"]
+textes = ["本文"]
+dates = ["日付"]
+views = ["View数"]
+likes = ["Like数"]
 filename = ARGV[0] + '.csv'
 countdown = $urls.length
 
-$urls.each_with_index do |url,i|
+$urls.each do |url|
     scrape_instagram(url)
     codes.push($code)
     textes.push($text)
     dates.push($date)
     views.push($view)
     likes.push($like)
-    puts('残り' + countdown.to_s + '件')
+
     countdown -= 1
+    puts('残り' + countdown.to_s + '件')
 end
 
 CSV.open(filename, "w") do |csv|
