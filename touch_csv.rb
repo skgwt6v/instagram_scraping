6d8d3c551d85b1dcd122b4e2cf980b79f6983669
skgwt6v <more_than_extend@yahoo.co.jp>
2017-12-01 13:30:18 +0900

    行列入れ替え

--- touch_csv.rb
+++ touch_csv.rb
@@ -11,31 +11,25 @@ require 'JSON'
 require './instagram_urls.rb'
 require './scrape_instagram.rb'
 
-codes = ["url"]
-textes = ["本文"]
-dates = ["日付"]
-views = ["View数"]
-likes = ["Like数"]
+
+stores = [["url","本文","日付","View数","Like数"]]
+
 filename = ARGV[0] + '.csv'
 countdown = $urls.length
 
 $urls.each do |url|
     scrape_instagram(url)
-    codes.push($code)
-    textes.push($text)
-    dates.push($date)
-    views.push($view)
-    likes.push($like)
+    array = []
+    array.push($code,$text,$date,$view,$like)
+    stores.push(array)
 
     countdown -= 1
     puts('残り' + countdown.to_s + '件')
 end
 
 CSV.open(filename, "w") do |csv|
-  csv << codes
-  csv << textes
-  csv << dates
-  csv << views
-  csv << likes
+    stores.each do |store|
+      csv << store
+    end
   puts ('success!')
 end
